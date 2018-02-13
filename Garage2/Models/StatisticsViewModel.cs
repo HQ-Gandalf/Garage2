f08ca58c34a08f5aa6c1f8fb240fd6cf0783a198
Erik Jon-And <erikjonand@gmail.com>
2018-02-13 11:36:39 +0100

    Statistics start

---
+++ Garage2/Models/StatisticsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class StatisticsViewModel
+    {
+        public int NoOfTrucks { get; set; }
+        public int NoOfBuses { get; set; }
+    }
+}
